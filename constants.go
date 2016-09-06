39386f26c15e2f8ff44f2bfbc2b510c9eee3e265
Raül Pérez <repejota@gmail.com>
2016-09-07 00:34:46 +0200

    Added constants

---
+++ constants.go
@@ -0,0 +1,15 @@
+package main
+
+const (
+	// Current username
+	USER = "\\u"
+
+	// Current hostname
+	HOSTNAME = "\\H"
+
+	// Current path
+	PATH = "\\w"
+
+	// Prompt : $ or users and # for root
+	PROMPT = "\\$"
+)
