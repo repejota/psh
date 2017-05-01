955fd86987933ab3a29d232b119e93a5c8620d3b
Raül Pérez <repejota@gmail.com>
2017-05-01 13:16:19 +0200

    smaller template

--- prompt/prompt.go
+++ prompt/prompt.go
@@ -58,7 +58,8 @@ func (p *Prompt) Render() string {
 	templateSource := ""
 	templateSource = templateSource + "{{color 0 108}}  {{.Shell.PromptEscapeJobs}} {{color 108 8}}"
 	templateSource = templateSource + "{{color 21 8}} {{.Shell.PromptEscapeCurentWorkingDirectory}} {{if .Git.IsAGitRepo}}{{color 8 19}}{{else}}{{color 8 0}}{{end}}"
-	templateSource = templateSource + "{{if .Git.IsAGitRepo}}{{color 21 19}}  {{.Git.Changes}} {{.Git.Branch}} {{color 19 0}}{{end}}{{reset}}"
+	templateSource = templateSource + "{{if .Git.IsAGitRepo}}{{color 21 19}}  {{.Git.Changes}} {{.Git.Branch}} {{color 19 0}}{{end}}"
+	templateSource = templateSource + "{{reset}}"
 	templateSource = templateSource + " "
 	tmpl, err := tmpl.Parse(templateSource)
 	if err != nil {
