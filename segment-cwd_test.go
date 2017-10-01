38a667f7fdf116db3c169238c093202e6e0fcf12
Raül Pérez <repejota@gmail.com>
2017-10-02 00:00:10 +0200

    Test segement cwd

--- segment-cwd_test.go
+++ segment-cwd_test.go
@@ -1,2 +1,13 @@
 // Copyright 2016-2017 The psh Authors. All rights reserved.
 package psh
+
+import "testing"
+
+func TestSegmentCWDCompile(t *testing.T) {
+	expected := "\\w"
+	segment := NewSegmentCWD()
+	segment.Compile()
+	if string(segment.Data) != expected {
+		t.Fatalf("Compiled data expected to be %s but got %s", expected, segment.Data)
+	}
+}
