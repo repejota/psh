ab6ecd7aa15c282d9fe1aa642273f759832ce270
Raül Pérez <repejota@gmail.com>
2018-01-12 21:18:18 +0100

    Added virtualenv to the defaults

--- defaults.go
+++ defaults.go
@@ -5,5 +5,5 @@ package psh
 const (
 	// DefaultSegments are tbe list of segments to show on the prompt by
 	// default.
-	DefaultSegments = "username,hostname,cwd,git,root"
+	DefaultSegments = "username,hostname,virtualenv,cwd,git,root"
 )
